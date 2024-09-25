b68ad76865641d7b68d8ec715c0b410f263599bb
prom12 <51293420+Prom12@users.noreply.github.com>
2024-09-25 19:30:48 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './component/NavBar/Navbar'
 import Hero from './component/Hero/Hero'
 import About from './component/About/About'
+import Services from './component/Services/Services'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
       <About/>
+      <Services/>
     </div>
   )
 }
---
+++ src/component/Services/Services.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Services.css.'
+import 
+const Services = () => {
+  return (
+    <div className='services'>
+        <div className="services-title">
+            <h1>My Services</h1>
+            <img src="" alt="" />
+        </div>
+    </div>
+  )
+}
+
+export default Services
