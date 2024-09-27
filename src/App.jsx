00ad61be9a58c129899a7f30c97b7f59a2a36cd4
prom12 <51293420+Prom12@users.noreply.github.com>
2024-09-27 10:54:19 +0000

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './component/NavBar/Navbar'
 import Hero from './component/Hero/Hero'
 import About from './component/About/About'
 import Services from './component/Services/Services'
+import MyWork from './component/MyWork/MyWork'
 const App = () => {
   return (
     <div>
@@ -10,6 +11,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Services/>
+      <MyWork/>
     </div>
   )
 }
---
+++ src/component/MyWork/MyWork.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './MyWork.css'
+import mywork_img from '../../assets/mywork_img.jpg'
+const MyWork = () => {
+  return (
+    <div className='mywork'>
+      <div className="mywork-title">
+        <h1>My Latest work</h1>
+        <img src={MyWork} alt=""/>
+      </div>
+    </div>
+  )
+}
+
+export default MyWork
