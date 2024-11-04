3f59913f23162c218249637d106916060913f3ef
prom12 <51293420+Prom12@users.noreply.github.com>
2024-11-04 12:33:24 +0000

    commit

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <div id='home' className='hero'>
       <img  style= {{borderRadius: '50%', width: 250}} src={profile_img} alt="profile" />
       <h1><span>I'm Clifford,</span> a front-end developer in Germany.</h1>
-      <p>I have an experience in front-end developing for 2 years.</p>
+      <p>I have an experience in frontend developing for 2 years.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
         <div className="hero-resume">My resume</div>
