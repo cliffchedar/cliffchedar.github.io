2cc5b7d95545eae68cbb6e20ac177306284202d4
clifchedar <ankrahjoe0@gmail.com>
2024-12-27 17:50:12 +0000

    commit

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
       <p>I have an experience in frontend developing for 2 years.</p>
       <div className="hero-action">
         <div className="hero-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+        <div className="hero-resume"><AnchorLink className='anchor-link' offset={50} href='#Clifford'>My resume</AnchorLink></div>
       </div>
     </div>
   )
